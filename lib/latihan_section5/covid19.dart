358d851697009969e3210f6df3716587f02ccdd5
frontend-adit <kakkooi63@gmail.com>
2022-02-10 23:02:50 +0700

    latihan section 5

--- lib/latihan_section5/covid19.dart
+++ lib/latihan_section5/covid19.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:coba/latihan/Models/Summary.dart';
+import './Models/Summary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
